8bf9757b452f557684a2029eb7b9b61c47376742
Heongilee <gjsrl1@gmail.com>
2020-10-28 16:00:05 +0900

    201028 16:00

--- Na_Dongbin_Algorithm_Youtube/Implementation_and_Simulation/04.py
+++ Na_Dongbin_Algorithm_Youtube/Implementation_and_Simulation/04.py
@@ -2,4 +2,16 @@ import sys
 sys.stdin = open(".\\Na_Dongbin_Algorithm_Youtube\\input.txt", "rt")
 
 if __name__ == "__main__":
-    pass
+    alpha = []
+    numeric = []
+    n = input()
+
+    cnt = 0
+    
+    for c in n:
+        if c.isalpha():
+            alpha.append(c)
+        else:
+            cnt += int(c)
+    alpha.sort()
+    print(''.join(alpha) + str(cnt))
