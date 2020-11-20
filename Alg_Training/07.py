e56d3725c4531ecfa2f3c5bf23c57a2b7b8aca87
Heongilee <gjsrl1@gmail.com>
2020-11-20 09:45:12 +0900

    201120 09:44; add solution at 07.py

--- Alg_Training/07.py
+++ Alg_Training/07.py
@@ -2,6 +2,7 @@ import sys
 from collections import deque
 sys.stdin = open(".\\Alg_Training\\input.txt", "rt")
 
+'''
 if __name__ == '__main__':
     dx = [-1, 0]    #12시 9시 방향.
     dy = [0, -1]
@@ -21,4 +22,22 @@ if __name__ == '__main__':
                     t = D[xx][yy]
             D[i][j] = t + board[i][j]
     print(D[N - 1][N - 1])
-            
+'''
+#######################################################################################
+# Solution
+##################################################################################
+if __name__ == '__main__':
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    dy = [[0] * n for _ in range(n)]
+    dy[0][0] = arr[0][0]
+    for i in range(1, n):
+        #Col
+        dy[0][i] = dy[0][i - 1] + arr[0][i]
+        #Row
+        dy[i][0] = dy[i - 1][0] + arr[i][0]
+    
+    for i in range(1, n):
+        for j in range(1, n):
+            dy[i][j] = min(dy[i - 1][j], dy[i][j - 1]) + arr[i][j]
+    print(dy[n - 1][n - 1])
