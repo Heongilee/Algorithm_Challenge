3dbbaaf02f00ba8761129614a70bd484772257be
Heongilee <gjsrl1@gmail.com>
2021-02-16 09:30:04 +0900

    210216 09:30

---
+++ acmicpc_net/210216/8958.py
@@ -0,0 +1,43 @@
+# 백준 8958번
+# https://www.acmicpc.net/problem/8958
+##################################################################
+# 내 풀이
+#############################################################
+import sys
+sys.stdin = open("./acmicpc_net/input.txt", "rt")
+
+if __name__ == '__main__':
+    tc = int(input())
+
+    for _ in range(tc):
+        res = 0
+        c = list(map(str, input()))
+        L = len(c)
+        lt = 0
+        rt = lt
+        while(rt < L):
+            while(rt < L and c[rt] != 'X'):
+                rt += 1
+            res += sum(range(1, rt - lt + 1))
+            while(rt < L and c[rt] != 'O'):
+                rt += 1
+            lt = rt
+        print(res)
+
+##################################################################
+# 답안 - ①
+#############################################################
+import sys
+sys.stdin = open("./acmicpc_net/input.txt", "rt")
+
+if __name__ == '__main__':
+    tc = int(input())
+
+    for _ in range(tc):
+        res = 0
+        c = list(map(str, input().split("X")))
+        
+        for i in range(len(c)):
+            t = c[i].count('O')
+            res += sum(range(1, t + 1))
+        print(res)
