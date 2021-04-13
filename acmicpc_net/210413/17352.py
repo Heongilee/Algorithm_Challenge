28add53c8c3f45fd8314d91b3567c85ab91f6df7
Heongilee <gjsrl1@gmail.com>
2021-04-13 09:07:57 +0900

    210413 09:07

---
+++ acmicpc_net/210413/17352.py
@@ -0,0 +1,33 @@
+import sys, itertools as it
+sys.stdin = open("./acmicpc_net/input.txt", "rt")
+input = sys.stdin.readline
+
+def findParent(v):
+    if v != parent[v]:
+        parent[v] = findParent(parent[v])
+    return parent[v]
+
+def unionParent(a, b):
+    a = findParent(a)
+    b = findParent(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+if __name__ == '__main__':
+    n = int(input())
+    parent = list(range(n + 1))
+    S = set()
+
+    for _ in range(n - 2):
+        a, b = map(int, input().split())
+
+        if findParent(a) != findParent(b):
+            unionParent(a, b)
+    
+    for x, y in it.combinations(range(1, n + 1), 2):
+        if findParent(x) != findParent(y):
+            print(x, y)
+            break
