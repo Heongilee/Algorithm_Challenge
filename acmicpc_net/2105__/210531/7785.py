5c9f0322c1a162a85f2c726eef4b92e8f5640dcb
Heongilee <gjsrl1@gmail.com>
2021-05-31 11:04:25 +0900

    210531 11:04

---
+++ acmicpc_net/2105__/210531/7785.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open("./acmicpc_net/input.txt", "rt")
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    n = int(input())
+    dic = dict()
+    for _ in range(n):
+        name, state = input().rstrip().split()
+        dic[name] = 1 if state == "enter" else 0
+    
+    res = []
+    for K, V in dic.items():
+        if V == 1: res.append(K)
+    print("\n".join(sorted(res, reverse=True)))
