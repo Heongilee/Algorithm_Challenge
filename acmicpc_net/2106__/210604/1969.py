6e510c4ce4bc321cd27ae349966c69a3e52c3965
Heongilee <gjsrl1@gmail.com>
2021-06-04 09:51:51 +0900

    210604 09:51

---
+++ acmicpc_net/2106__/210604/1969.py
@@ -0,0 +1,29 @@
+import sys
+from collections import defaultdict
+sys.stdin = open("./acmicpc_net/input.txt", "rt")
+input = sys.stdin.readline
+
+INF = int(10e9)
+if __name__ == '__main__':
+    # N은 1,000보다 작거나 같은 자연수이고, M은 50보다 작거나 같은 자연수
+    n, m = map(int, input().split())
+    board = [[0] * n for _ in range(n)] # <행>과 <열>의 HammingDist 길이는 board[i][j]
+    dnas = [input().rstrip() for _ in range(n)]
+    hammingDist = 0
+    S = ""
+    
+    for i in range(m):
+        myList = []
+        tmp = [dnas[j][i] for j in range(n)]
+        dic = defaultdict(int)
+        for e in tmp: dic[e] += 1
+        for k, v in dic.items():
+            myList.append((k, v))
+        
+        myList.sort(key=lambda X: (-X[1], X[0]))
+        hammingDist += sum(map(lambda j : 1 if myList[0][0] != tmp[j] else 0, range(n)))
+        S += myList[0][0]
+    print(S)
+    print(hammingDist)
+
+        
