8c44d076f341f4f26a8a39dc76f6f46ff2f86231
Heongilee <gjsrl1@gmail.com>
2021-06-15 10:51:14 +0900

    210615 10:51

--- acmicpc_net/2106__/210610/14712.py
+++ acmicpc_net/2106__/210610/14712.py
@@ -3,38 +3,47 @@ sys.stdin = open("./acmicpc_net/input.txt", "rt")
 sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
-dx = [0, 0, -1, -1]
-dy = [0, -1, 0, -1]
-def DFS(i, j):
-    global cnt
+##############################################################################
+# Solution (1% TLE in Python3, MLE in Pypy3)
+#########################################################################
+def check(r, c):
+    return board[r - 1][c] and board[r][c - 1] and board[r - 1][c - 1]
 
-    if i == n - 1 and j == m:
+def DFS(r, c):
+    global ans
+    if r == n and c == m + 1:
         print([b for b in board])
-        cnt += 1
+        ans += 1
         return
+    
+    for i in range(r, n + 1):
+        for j in range((c if i == r else 1), m + 1):
+            '''
+            이 조건에 걸리게 되면,,,
 
-    for k in range(i, n):
-        for l in range((j if i == k else 0), m):
-            if k > 0 and l > 0:
-                if all (board[k + dx[m]][l + dy[m]] for m in range(4)): continue
-            board[k][l] = 1
-            DFS(k, l + 1)
+            [1, 1]
+            [1, X]      # X는 Don't care
+            
+            를 만나게 됐을 때인데, 여기서 하나만 1로 체크 돼서 가지를 뻗어 나가면
+            2x2 넴모넴모가 만들어지게 되므로 더 뻗어 나가지 않도록 앞에서 
+            continue를 걸어주는 것이다. 
+            '''
+            if check(i, j): continue
 
-            board[k][l] = 0
-            DFS(k, l + 1)
-
+            board[i][j] = 1
+            DFS(i, j + 1)
+            board[i][j] = 0
+    print([b for b in board])
+    ans += 1
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    board = [[0] * m for _ in range(n)]
-    cnt = 0
+    board = [[0] * (m + 1) for _ in range(n + 1)]
+    ans = 0
 
-    board[0][0] = 1
-    DFS(0, 1)
-    board[0][0] = 0
-    DFS(0, 1)
-
-    print(2 ** (n * m), cnt)
+    DFS(1, 1)
+    print(ans)
+    
 
 ##############################################################################
 # Python MLE, Pypy3 TLE
