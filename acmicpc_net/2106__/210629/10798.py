3fbb526dc2b10144dd56711ea175942e2fa08989
Heongilee <gjsrl1@gmail.com>
2021-06-30 11:02:07 +0900

    ~ 210629

--- acmicpc_net/2106__/210629/10798.py
+++ acmicpc_net/2106__/210629/10798.py
@@ -3,16 +3,9 @@ sys.stdin = open("./acmicpc_net/input.txt", "rt")
 input = sys.stdin.readline
 
 if __name__ == '__main__':
-    col = 0
-    board = []
-    
+    arr = []
     for _ in range(5):
         t = list(input().rstrip())
-        if len(t) > col: col = len(t)
         while len(t) != 15: t.append("")
-        board.append(t)
-
-    for c in range(col):
-        for r in range(5):
-            print(board[r][c], end='')
-    print()
+        arr.append(t)
+    print("".join([arr[r][c] for c in range(15) for r in range(5)]))
