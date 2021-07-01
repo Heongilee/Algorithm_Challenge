b376e7d9d54b2f63a7a45fbe1ea806d70e80256c
Heongilee <gjsrl1@gmail.com>
2021-07-01 14:07:41 +0900

    BOJ 2160 :: 그림비교

--- acmicpc_net/2106__/210630/5639.py
+++ acmicpc_net/2106__/210630/5639.py
@@ -1,37 +1,38 @@
 import sys
 sys.stdin = open("./acmicpc_net/input.txt", "rt")
-sys.setrecursionlimit(10 ** 6)
+sys.setrecursionlimit(10 ** 9)
 input = sys.stdin.readline
 ##################################################################################################
-# 27% TLE
+# 29% TLE
 #############################################################################################
 class Node(object):
-    def __init__(self, data = None):
-        self.data = data # 양수형 정수
-        self.children = [None, None]
-        # self.parent = None
+    def __init__(self, data=None):
+        self.data = data                # 양수형 정수
+        self.children = [None, None]    # [왼쪽 자식, 오른쪽 자식]
+        self.parent = None              # 부모 노드를 가리킴
 
 class Tree(object):
     def __init__(self, initNode):
         self.root = Node(initNode)
         self.curNode = self.root
-        self.pointer = None
-    
-    # ! 1만번의 입력마다 O(logN)을 수행하면 시간이 낭비될 수 있음.
+        self.pointer = None             # 후위 순회용 포인터
+
     def insertNumber(self, n):
-        self.curNode = self.root
+        while self.curNode.parent != None and n > self.curNode.parent.data:
+            self.curNode = self.curNode.parent
         while True:
             if n < self.curNode.data:
                 if self.curNode.children[0] == None:
                     self.curNode.children[0] = Node(n)
+                    self.curNode.children[0].parent = self.curNode
                     break
                 self.curNode = self.curNode.children[0]
             elif n > self.curNode.data:
                 if self.curNode.children[1] == None:
                     self.curNode.children[1] = Node(n)
+                    self.curNode.children[1].parent = self.curNode
                     break
                 self.curNode = self.curNode.children[1]
-
 
     def preorderTraversal(self, ptr):
         pNode = self.pointer
@@ -39,18 +40,18 @@ class Tree(object):
         if self.pointer == None:
             self.pointer = pNode
             return
-        
+
         self.preorderTraversal(self.pointer.children[0])
         self.preorderTraversal(self.pointer.children[1])
         print(self.pointer.data)
         self.pointer = pNode
-            
+
+
 if __name__ == '__main__':
     tree = Tree(int(input()))
-    for number in sys.stdin: 
-        tree.insertNumber(int(number))
+    for number in sys.stdin: tree.insertNumber(int(number))
     tree.preorderTraversal(tree.root)
-    
+
 
 ##################################################################################################
 # RE(Index Error) :: 노드가 10000개 까지 올 수 있기 때문에, 배열크기는 2^10000이 필요하게 된다.
